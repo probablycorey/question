2abf3352a17ac0ead4a72e83e79270ff0a00301a
probablycorey <cj@github.com>
2015-01-13 08:31:33 -0800

    The x-games are kinda of entertaining

--- interrogate.rb
+++ interrogate.rb
@@ -1,33 +1,81 @@
-# ◉◯
-
 require 'bundler/setup'
 require 'colorize'
-require 'readline'
-require 'tempfile'
+require 'io/console'
+require 'timeout'
 
-tmp_file = Tempfile.new('hello')
+module KEYS
+  SPACE = " "
+  TAB = "\t"
+  RETURN = "\r"
+  LINEFEED = "\n"
+  ESCAPE = "\e"
+  UP = "\e[A"
+  DOWN = "\e[B"
+  RIGHT = "\e[C"
+  LEFT = "\e[D"
+  BACKSPACE = "\177"
+  SIGINT = "\003"
+end
 
-Readline.output = File.open(tmp_file, "w+")
+module ANSI
+  HIDE = "\e[?25l"
+  SHOW = "\e[?25h"
+  CLEAR_DOWN = "\e[0J"
+end
 
-count = 0
-Thread.new do
-  input = open(tmp_file, "r+")
-  while true
-    character = input.getc
-    if character == "!"
-      count += 1
-      $stdout.write(character)
-      $stdout.flush
+OUTPUT = $stdout
+INPUT = $stdin
+
+def get_input()
+  input = INPUT.getch()
+  if input == "\e"
+    Timeout::timeout(1) do
+      input += INPUT.getch()
+      input += INPUT.getch()
+    end
+  end
+  input
+rescue Timeout::Error
+  input
+end
+
+def hide_cursor(&block)
+  sync_value = OUTPUT.sync
+  OUTPUT.sync = true
+
+  OUTPUT.print ANSI::HIDE
+  block.call()
+  OUTPUT.print ANSI::SHOW
+  OUTPUT.sync = sync_value
+end
+
+def ask(question)
+  OUTPUT.print question + ": "
+
+  hide_cursor do
+    loop do
+      input = get_input()
+      OUTPUT.puts input.chars.length
+      case input
+      when KEYS::SIGINT
+        exit 130
+      when KEYS::RETURN
+        puts "DONE!"
+        exit 1
+      when KEYS::ESCAPE
+        print "ESCAPE!"
+      when KEYS::DOWN
+        print KEYS::DOWN
+        print "HI"
+        print KEYS::UP
+      when KEYS::UP
+        print "!"
+        print ANSI::CLEAR_DOWN
+      when /^[^\e]/
+        OUTPUT.print input
+      end
     end
   end
 end
 
-begin
-  while line = Readline.readline("", true)
-    count = 0
-    $stdout.write(count)
-    $stdout.flush
-  end
-rescue EOFError
-  puts "Exiting..."
-end
+ask "How are you"
