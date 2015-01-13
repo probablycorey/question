cb2ec579d4c23677c4ec009acebe1904fa44fe63
probablycorey <cj@github.com>
2015-01-13 15:10:02 -0800

    Add radio question

--- interrogate.rb
+++ interrogate.rb
@@ -1,15 +1,31 @@
 require 'bundler/setup'
+
 require 'pp'
 
+require 'colorize'
+require 'io/console'
+require 'timeout'
+
+require_relative("./tty")
 require_relative "./checkbox"
+require_relative "./radio"
 
 choices = [
+  { label: "good", value: 1 },
+  { label: "good", value: 1 },
+  { label: "neutral", value: 0 },
+  { label: "horrible", value: -1 },
+  { label: "good", value: 1 },
+  { label: "neutral", value: 0 },
+  { label: "neutral", value: 0 },
+  { label: "horrible", value: -1 },
   { label: "good", value: 1 },
   { label: "neutral", value: 0 },
   { label: "horrible", value: -1 }
 ]
 
-checkbox = Checkbox.new("How are you", choices)
-result = checkbox.ask
+question = Checkbox.new("How are you", choices)
+question = Radio.new("How are you", choices)
+result = question.ask
 
 pp result
