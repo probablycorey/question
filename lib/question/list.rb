900ddeeba5ab4526397cba481fc705b249c48950
probablycorey <cj@github.com>
2015-01-15 13:15:02 -0800

    Add emacs/vi up/down keys to list

--- lib/question/list.rb
+++ lib/question/list.rb
@@ -33,10 +33,10 @@ module Question
         exit 130
       when TTY::CODE::RETURN, TTY::CODE::SPACE
         @finished = true
-      when TTY::CODE::DOWN
+      when TTY::CODE::DOWN, TTY::CODE::CTRL_J, TTY::CODE::CTRL_N
         @active_index += 1
         @active_index = 0 if @active_index >= @choices.length
-      when TTY::CODE::UP
+      when TTY::CODE::UP, TTY::CODE::CTRL_K, TTY::CODE::CTRL_P
         @active_index -= 1
         @active_index = @choices.length - 1 if @active_index < 0
       end
