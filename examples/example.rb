37903da185a081fa9d0e93b6af417eb413461c66
probablycorey <cj@github.com>
2015-01-15 10:30:33 -0800

    Don’t require the Question gem to be installed to run the example

--- examples/example.rb
+++ examples/example.rb
@@ -1,11 +1,12 @@
-require "pp"
+$:.unshift File.expand_path("../../lib", __FILE__)
 
+require "pp"
 require "question"
 
 choices = [
   { label: "red", value: {r: 255, g: 0, b: 0} },
   { label: "blue", value: "#0000FF" },
-  { label: "green", value: "green" }
+  "green" # You can also just pass a String or Number
 ]
 
 question = Question::CheckboxList.new("What colors do you like", choices, default: choices[1..-1])
